100f74462f943f286c9bde2e8279cc02357e87b4
= <=>
2022-08-17 03:17:02 +0300

    added css grid for mobile and pc compaitablity better pdf and screenshot header added

--- javascript/main.js
+++ javascript/main.js
@@ -3,11 +3,11 @@ window.jsPDF = window.jspdf.jsPDF
 
 const courses = document.querySelectorAll('.course')
 const courseText = Array.from(courses).map(x=> x.querySelector('h2').innerText)
-console.log(courseText)
+
 
 let coursePdfHour = Array.from(courses).find(x=> x.querySelector('h2').innerText === 'Math 101') 
-console.log(coursePdfHour)
-let colors = ['rgb(255, 255, 255)' , 'rgb(0, 0, 0)' , 'rgb(142, 202, 230)' , 'rgb(2, 195, 154)'  , 'rgb(255, 183, 3)']
+
+let colors = ['rgb(255, 255, 255)' , 'rgb(181, 126, 220)' , 'rgb(142, 202, 230)' , 'rgb(2, 195, 154)'  , 'rgb(255, 183, 3)']
 
 let hours  ={
     notYetHours:128,
@@ -25,7 +25,7 @@ let coursesSelected = {
     termThreeCourses:[],
 }
 coursesSelected['notYetCourses'] = courseText
-console.log(coursesSelected)
+
 let two = []
 const notYetH = document.querySelector('.notYetH')
 const finishedH = document.querySelector('.finishedH')
@@ -48,8 +48,7 @@ courses.forEach(course => {
             two = checkForHours(course , hours.notYetHours , hours.finishedHours)
             hours.notYetHours = two[0]
             hours.finishedHours = two[1]
-            console.log(two)
-            console.log(coursesSelected)
+
         }
         else if(course.style.backgroundColor == colors[1] ){
             course.style.backgroundColor = colors[2]
@@ -112,10 +111,10 @@ year.addEventListener('change', changeTerm)
 
 function changeTerm(){
     
-    const arrYear = year.value 
-    firstTerm.textContent = ":  " + arrYear + 1
-    secondTerm.textContent = ":  " + arrYear + 2
-    thirdTerm.textContent = ":  " + arrYear + 3
+    const arrYear = year.value.toString() 
+    firstTerm.textContent =  arrYear + 1
+    secondTerm.textContent =  arrYear + 2
+    thirdTerm.textContent =  arrYear + 3
    
 }
 
@@ -136,7 +135,7 @@ function checkForHours(course , from , to){
         to += 1
         from -= 1 
     }
-    console.log(`from = ${from} to = ${to}`)
+    
     return [from , to]
 }
 
@@ -148,93 +147,47 @@ document.querySelector('#tutorialButton').addEventListener('click',hideContent =
 
 let imgButton = document.querySelector('#exportImg')
 
-imgButton.addEventListener('click' ,_ =>{
+imgButton.addEventListener('click' ,()=>{
 
+    const elementToSave = document.querySelector(".gridContainer");
+       
+    html2canvas(elementToSave, {
+        allowTaint: true,
+        foreignObjectRendering: true,
+        scale : 3
+    }).then(canvas =>{
+                const a = document.createElement("a");
+                a.href = canvas.toDataURL("image/jpeg");
+                a.download = "Plan.jpeg";
+                a.click();
+            })})
 
-    html2canvas(document.body).then((canvas) => {
-        let a = document.createElement("a");
-        a.download =  "ss.png";
-        a.href = canvas.toDataURL("image/png");
-        a.click();
-      }); 
-  });
 
 let pdfButton = document.querySelector('#exportPdf')
 
 pdfButton.addEventListener('click' , makePdf)
 
-let textLiner = 2
+var textLiner1 = 2
+var textLiner2 = 10
+var textLiner3 = 15
 
 async function makePdf(){
     const newDoc = await new jsPDF()
-    a = await pdfWrite(newDoc,'termOneCourses' , 'first term' , 'firstTermHours')
-    dd = await newDoc.addPage()
-    b = await pdfWrite(newDoc,'termTwoCourses' , 'second term' , 'secondeTermHours')
-    dd = await newDoc.addPage()
-    c = await pdfWrite(newDoc,'termThreeCourses' , 'third term' , 'thirdTermHours')
-    dd = await newDoc.addPage()
-    d = await pdfWrite(newDoc,'finishedCourses' , 'finished courses' , 'finishedHours')
-    dd = await newDoc.addPage()
-    e = await pdfWrite(newDoc,'notYetCourses' , 'not yet courses' , 'notYetHours')
-     
+    pdfWrite(newDoc,'termOneCourses' , 'first term' , 'firstTermHours' , textLiner1 )
+  
+    pdfWrite(newDoc,'termTwoCourses' , 'second term' , 'secondeTermHours' , textLiner2)
+
+    pdfWrite(newDoc,'termThreeCourses' , 'third term' , 'thirdTermHours' , textLiner3)
+    newDoc.addPage()
+
+    pdfWrite(newDoc,'finishedCourses' , 'finished courses' , 'finishedHours')
+    newDoc.addPage()
     
-    //  coursesSelected['termOneCourses'].forEach((x,i) => {
-    //     newDoc.text('first term courses' , 10 ,10)
-    //     newDoc.text(`${x}\n`,10,10*(textLiner))
-    //     newDoc.text(`${findHour(x)}`,40,10*(textLiner))
-    //     if(i != 0 && i % 20 ===0 ){
-    //         newDoc.addPage()
-    //         textLiner = 2
-    //     }
-    //     textLiner++
-    //     console.log('1')
-    // })
-    // coursesSelected['termTwoCourses'].forEach((x,i) => {
-    //     newDoc.text('second term courses' , 10 ,10)
-    //     newDoc.text(`${x}\n`,10,10*(textLiner))
-    //     newDoc.text(`${findHour(x)}`,40,10*(textLiner))
-    //     if(i != 0 && i % 20 ===0 ){
-    //         newDoc.addPage()
-    //         textLiner = 2
-    //     }
-    //     textLiner++
-    //     console.log('1')
-    // })
-    // coursesSelected['termThreeCourses'].forEach((x,i) => {
-    //     newDoc.text('summer term courses' , 10 ,10)
-    //     newDoc.text(`${x}\n`,10,10*(textLiner))
-    //     newDoc.text(`${findHour(x)}`,40,10*(textLiner))
-    //     if(i != 0 && i % 20 ===0 ){
-    //         newDoc.addPage()
-    //         textLiner = 2
-    //     }
-    //     textLiner++
-    //     console.log('1')
-    // })
-    // coursesSelected['finishedCourses'].forEach((x,i) => {
-    //     newDoc.text('finished courses' , 10 ,10)
-    //     newDoc.text(`${x}\n`,10,10*(textLiner))
-    //     newDoc.text(`${findHour(x)}`,40,10*(textLiner))
-    //     if(i != 0 && i % 20 ===0 ){
-    //         newDoc.addPage()
-    //         textLiner = 2
-    //     }
-    //     textLiner++
-    //     console.log('1')
-    // })
-    // coursesSelected['notYetCourses'].forEach((x,i) => {
-    //     newDoc.text('not yet courses' , 10 ,10)
-    //     newDoc.text(`${x}\n`,10,10*(textLiner))
-    //     newDoc.text(`${findHour(x)}`,40,10*(textLiner))
-    //     if(i != 0 && i % 20 ===0 ){
-    //         newDoc.addPage()
-    //         textLiner = 2
-    //     }
-    //     textLiner++
-    //     console.log('1')
-    // })
+    pdfWrite(newDoc,'notYetCourses' , 'not yet courses' , 'notYetHours')
+    
+    
+
     newDoc.save('new.pdf')
-    
 }
 
 
@@ -255,21 +208,44 @@ function findHour(course){
     }
     return hour
 }
-async function pdfWrite(pdfName , term ,headerText , termHours ){
-    let textLiner = 2
+async function pdfWrite(pdfName , term ,headerText , termHours , textLiner = 2 ){
+    
     let j = 10
-    pdfName.text(`${headerText}` , 10 ,10)
+    pdfName.text(`${headerText} : ` , 10 ,textLiner*10)
+    textLiner++
     coursesSelected[`${term}`].forEach((x,i) => {
         pdfName.text(`${x}\n`,j , 10*(textLiner))
         pdfName.text(`${findHour(x)}`,j+30,10*(textLiner))
-
-        textLiner++
-        if(i != 0 && i % 20 === 0 ){
-            // pdfName.addPage()
-            j += 30 + 20
-            textLiner = 2
+    
+        // textLiner++
+        j += 50
+        if(j === 160){
+            j =10
+            textLiner++
         }
-        console.log('1')
+   
+
     })
-    pdfName.text(`sum of hours = ${hours[termHours]} ` ,j,10*(textLiner))
+    pdfName.text(`sum of hours = ${hours[termHours]} ` ,10,10*(textLiner+1))
+    
 }
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+      link.href = uri;
+      link.download = filename;
+
+      //Firefox requires the link to be in the body
+      document.body.appendChild(link);
+
+      //simulate click
+      link.click();
+
+      //remove the link when done
+      document.body.removeChild(link);
+    } else {
+      window.open(uri);
+    }
+  }
+
+
